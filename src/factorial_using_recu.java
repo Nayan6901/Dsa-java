a341d6dff9c7721ab065ebca9d53e5caf73dff51
Nayan6901 <dhavannayan@gmail.com>
2025-02-25 12:08:46 +0530

    changes

--- src/factorial_using_recu.java
+++ src/factorial_using_recu.java
@@ -2,4 +2,16 @@
 
 public class factorial_using_recu{
 
+    public static int fact(int n){
+        if(n<2){
+            return n;
+        }
+        return n*fact(n-1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+
 }
