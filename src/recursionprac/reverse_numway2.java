3aa7fd1f679cb830bba7255eb12ba011dee7dff5
Nayan6901 <dhavannayan@gmail.com>
2025-02-26 17:54:54 +0530

    changes

--- src/recursionprac/reverse_numway2.java
+++ src/recursionprac/reverse_numway2.java
@@ -11,6 +11,7 @@ public class reverse_numway2 {
     private static int helper(int n,int digits) {
         int rem=n%10;
         if (n % 10 == n){
+
             return n;
         }
         return rem*(int)(Math.pow(10,digits-1))+helper(n/10,digits-1);
